76415e3e4c6ea990290c7adf957625fd7a365dce
Xander Davis <xdd5007@psu.edu>
2026-02-08 12:09:41 -0500

    Lab03

--- lab03/lab03.md
+++ lab03/lab03.md
@@ -1,2 +1,11 @@
-# Prompt Engineering Process
+#### Intention
+>I plan on increasing the tempurature of the model
 
+#### Action/Change
+>I think it will improve the agent because it will increase the quality and quanitity of the response to the question
+
+#### Result
+>It gave a more in depth answer to the responce
+
+#### Reflection/Analysis of the result. 
+>I think it worked in making the model better. I think this because it improved the quality of the response and gave a better answer then when the tempurature was on .5
--- lab03/lab03_dnd_agent.py
+++ lab03/lab03_dnd_agent.py
@@ -8,8 +8,8 @@ from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
 # Add you code below
 sign_your_name = 'Pulin Agrawal'
 model = 'gemma3:270m'
-options = {}
-messages = []
+options = {'temperature': 1, 'max_tokens': 100}
+messages = [{'role': 'system', 'content': f"You are a Dungeons & Dragons (DND) game master named 'Eldor the Wise'. You are creative and imaginative, crafting engaging narratives and challenges for players. You should respond to player actions with vivid descriptions, plot twists, and character interactions. Your goal is to create an immersive and enjoyable experience for the players. Always stay in character as Eldor the Wise, the DND game master."}]
 
 
 # But before here.
@@ -18,16 +18,16 @@ messages.append({'role':'user', 'content':''}) # An empty user message to prompt
 options |= {'seed': seed(sign_your_name)}
 # Chat loop
 while True:
-  response = chat(model=model, messages=messages, stream=False, options=options)
-  # Add your code below
-
-
-  # But before here.
-  if messages[-1]['content'] == '/exit':
-    break
+ response = chat(model=model, messages=messages, stream=False, options=options) 
+ user_text = input('You: ')
+ messages.append(response['message']) 
+ print(f'Agent: {response["message"]["content"]}')
+ messages.append({'role': 'user', 'content': user_text}) 
+ if messages[-1]['content'] == '/exit':
+  break
 
 # Save chat
-with open(Path('lab03/attempts.txt'), 'a') as f:
+with open(Path('attempts.txt'), 'a') as f:
   file_string  = ''
   file_string +=       '-------------------------NEW ATTEMPT-------------------------\n\n\n'
   file_string += f'Model: {model}\n'
